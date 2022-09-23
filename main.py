8e77c6d2e24eda046b0bbb74bc20d2726881e3eb
Sebastyk120 <90669455+Sebastyk120@users.noreply.github.com>
2022-09-23 13:26:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,102 @@
 import datetime
 
-# from datetime import datetime
+"""----------------------------------------- Declaracion de variables -----------------------------------------------"""
+jornada_ordinaria_horas = 8
 
-fechaactual = datetime.datetime.now()
-print(fechaactual)
+"""-----------------------------------Recolectores De Floats Horas --------------------------------------------------"""
 
-fecha = datetime.datetime(2022, 10, 1)
-fecha1 = datetime.datetime(2022, 10, 1, 23, 45)
-print(fecha1)
-
-fechaactual2 = datetime.datetime.strftime(fechaactual, '%d/%m/%Y %H:%M:%S')
-print(fechaactual2)
-
-fechaactual3 = datetime.datetime.strftime(fechaactual, '%b %d %Y %H:%M:%S')
-print(fechaactual3)
-
-fechaTexto = "Dec 06 2022 22:10"
-fechaactual4 = datetime.datetime.strptime(fechaTexto, '%b %d %Y %H:%M')
-print(fechaactual4)
-
-dia = int(datetime.datetime.strftime(fechaactual, '%d'))
-print(dia)
-
-horaactual = datetime.datetime.strftime(fechaactual, '%H:%M:%S')
-print(horaactual)
-
-fechapasada = datetime.datetime(2022, 9, 1)
-diferencia = fechaactual - fechapasada
-print(diferencia)
-print(diferencia.days)
-print(diferencia.total_seconds())
-
-dia_delta = datetime.timedelta(days=5)
-fechainicial = datetime.date.today()
-print(fechaactual)
-fechafutura = fechainicial + dia_delta
-print(fechafutura)
-
-fecha5 = datetime.datetime.now().isoformat()
-print(fecha5)
-
-# Recolectar fecha de inicio y hora
-fecha_hora_vacia = input("Ingrese la fecha y hora --- Ejemplo 17/03/2022 13:30: ")
-fecha_hora_vacia2 = datetime.datetime.strptime(fecha_hora_vacia, '%d/%m/%Y %H:%M')
-print(fecha_hora_vacia2)
+"""Recolectar fecha de inicio y hora"""
 
 
-def horainicio():
-    fecha_nom_ijd = datetime.datetime(2022, 3, 26, 13, 30)
-    hora = datetime.datetime.strftime(fecha_nom_ijd, '%H')
-    hora = int(hora)
-    minutos = datetime.datetime.strftime(fecha_nom_ijd, '%M')
+def hora_inicio_jornada():
+    inicio_jornada_vacia = input("Ingrese la fecha y hora inicio jornada - Ejemplo 17/03/2022 13:30: ")
+    inicio_jornada_vacia = datetime.datetime.strptime(inicio_jornada_vacia, '%d/%m/%Y %H:%M')
+    hora_i = datetime.datetime.strftime(inicio_jornada_vacia, '%H')
+    hora_i = int(hora_i)
+    minutos = datetime.datetime.strftime(inicio_jornada_vacia, '%M')
     minutos = round(float(minutos) / 60, 1)
-    horarioentrada = hora + minutos
-    return horarioentrada
+    horarioentrada = hora_i + minutos
+    return horarioentrada, hora_i
 
 
-vacio = None
-horainicio2 = horainicio()
-print(horainicio2)
+i, hora_i_i = hora_inicio_jornada()
+
+"""Recolectar fecha de inicio descanso y hora"""
+
+
+def hora_inicio_descanso():
+    inicio_descanso_vacia = input(
+        "Ingrese la fecha y hora inicio descanso (Si no  hay descanso escriba N/A) - Ejemplo 17/03/2022 13:30: ")
+    if inicio_descanso_vacia == "N/A":
+        return inicio_descanso_vacia
+    else:
+        inicio_descanso_vacia = datetime.datetime.strptime(inicio_descanso_vacia, '%d/%m/%Y %H:%M')
+        hora_i_d = datetime.datetime.strftime(inicio_descanso_vacia, '%H')
+        hora_i_d = int(hora_i_d)
+        minutos = datetime.datetime.strftime(inicio_descanso_vacia, '%M')
+        minutos = round(float(minutos) / 60, 1)
+        horario_entrada_descanso = hora_i_d + minutos
+        return horario_entrada_descanso, hora_i_d
+
+
+d, hora_i_d_d = hora_inicio_descanso()
+
+"""Recolectar fecha de salida descanso y hora"""
+
+
+def hora_salida_descanso():
+    salida_descanso_vacia = input(
+        "Ingrese la fecha y hora salida descanso (Si no  hay descanso escriba N/A)- Ejemplo 17/03/2022 13:30: ")
+    if salida_descanso_vacia == "N/A":
+        return salida_descanso_vacia
+    else:
+        salida_descanso_vacia = datetime.datetime.strptime(salida_descanso_vacia, '%d/%m/%Y %H:%M')
+        hora_s_d = datetime.datetime.strftime(salida_descanso_vacia, '%H')
+        hora_s_d = int(hora_s_d)
+        minutos = datetime.datetime.strftime(salida_descanso_vacia, '%M')
+        minutos = round(float(minutos) / 60, 1)
+        salida_descanso_vacia = hora_s_d + minutos
+        return salida_descanso_vacia, hora_s_d
+
+
+r, hora_s_d_d = hora_salida_descanso()
+
+"""Recolectar fecha de salida jornada y hora"""
+
+
+def hora_salida_jornada():
+    salida_jornada_vacia = input("Ingrese la fecha y hora salida jornada - Ejemplo 17/03/2022 13:30: ")
+    salida_jornada_vacia = datetime.datetime.strptime(salida_jornada_vacia, '%d/%m/%Y %H:%M')
+    hora_s = datetime.datetime.strftime(salida_jornada_vacia, '%H')
+    hora_s = int(hora_s)
+    minutos = datetime.datetime.strftime(salida_jornada_vacia, '%M')
+    minutos = round(float(minutos) / 60, 1)
+    salida_jornada_vacia = hora_s + minutos
+    return salida_jornada_vacia, hora_s
+
+
+s, hora_s_s = hora_salida_jornada()
+
+"""-----------------------------------Funcion Ingreso A Descanso --------------------------------------------------"""
+
+
+def laboradas_azul():
+    if hora_i_d_d != "N/A" and hora_s_d_d != "N/A":
+        if hora_i_d_d > hora_i_i:
+            laboradas = hora_i_d_d - hora_i_i
+            return laboradas
+        else:
+            if hora_i_d_d < hora_i_i:
+                laboradas = 24 - hora_i_i + hora_i_d_d
+                return laboradas
+            else:
+                laboradas = 0
+                return laboradas
+    else:
+        laboradas = None
+        return laboradas
+laboradas2 = laboradas_azul()
+print(laboradas2)
+
 """if horainicio != 0:
     if horainicio2 ==0 and horainicio2 == 1:"""
