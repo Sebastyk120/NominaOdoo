06624a358160788e4dad1ddfa2b49d646741a563
SebastianM <Sebastyk120@gmail.com>
2022-09-29 21:36:42 -0500

    Se añaden declaraciones de horas flotantes.

--- Transportes3.py
+++ Transportes3.py
@@ -6,20 +6,42 @@ workbook = load_workbook(filename="ProgramacionTransporte.xlsx")
 # open workbook
 sheet = workbook.active
 
-Rango_condicion = sheet['A1':'M308']
+rango_condicion = sheet['A1':'M308']
 rango_Columna = sheet['L1':'L308']
 
 contador = -1
-Proveedor = "Proveedor"
-Nombre = "Nombre"
+ide = "id"
+tipo_de_servicio = "tipo_de_servicio"
+fecha_servicio = "tipo_de_servicio"
+hora_servicio = "hora_servicio"
+producto = "producto"
+cantidad_producto = "cantidad_producto"
+kilogramos = "kilogramos"
+origen = "origen"
+destino = "destino"
+proveedor = "proveedor"
+celular_proveedor = "celular_proveedor"
+conductor = "conductor"
+celular_conductor = "celular_conductor"
 
 
 for i in rango_Columna:
     contador += 1
     if i[0].value == "Hola":
         i[0].value = "Hola"
-        Proveedor = Rango_condicion[contador][12].value
-        Nombre = Rango_condicion[contador][14].value
+        ide = rango_condicion[contador][0].value
+        tipo_de_servicio = rango_condicion[contador][5].value
+        fecha_servicio = rango_condicion[contador][7].value
+        hora_servicio = rango_condicion[contador][8].value
+        producto = rango_condicion[contador][9].value
+        cantidad_producto = rango_condicion[contador][10].value
+        kilogramos = rango_condicion[contador][11].value
+        origen = rango_condicion[contador][12].value
+        destino = rango_condicion[contador][13].value
+        proveedor = rango_condicion[contador][14].value
+        celular_proveedor = rango_condicion[contador][15].value
+        conductor = rango_condicion[contador][16].value
+        celular_conductor = rango_condicion[contador][17].value
 
 # modify he desired cell
 # sheet["L6"] = "Ejecutado123"
--- trasnportes.py
+++ trasnportes.py
@@ -1,14 +1,14 @@
-import numpy as np
+"""import numpy as np
 import pandas as pd
 <<<<<<< HEAD
 =======
 import xlsxwriter
->>>>>>> 6988272 (Se añaden declaraciones de horas flotantes.)
+
 import openpyxl
 
 # Lectura del archivo .xlsx
 
-<<<<<<< HEAD
+
 transportes_df = pd.read_excel('ProgramaciónTransporte.xlsx',sheet_name='Resultado')
 
 # Mostrar libro
@@ -21,7 +21,7 @@ transportes_df = pd.read_excel('ProgramacionTransporte.xlsx', sheet_name='Result
 # Mostrar libro
 
 # transportes_df['salida'] = transportes_df.mean(axis=1)
->>>>>>> 6988272 (Se añaden declaraciones de horas flotantes.)
+
 
 # transportes_df['Enviado'] = np.where(transportes_df['media'] >20, 'Ok', 'Nok')
 
@@ -40,8 +40,8 @@ transportes_df.to_excel(excel_writer='ProgramaciónTransporte.xlsx', sheet_name=
 
 
 
-=======
+
 print(transportes_df)
 transportes_df.to_excel('ProgramacionTransporte.xlsx', sheet_name='Resultado2')
 transportes_df.to_excel('ProgramacionTransporte.xlsx', sheet_name='Resultado2', engine='xlsxwriter')
->>>>>>> 6988272 (Se añaden declaraciones de horas flotantes.)
+
