e5ad6556f826b6b8438304392b95b9ad73c978d8
SebastianM <Sebastyk120@gmail.com>
2022-09-29 21:36:42 -0500

    Se añaden declaraciones de horas flotantes.

--- Transportes3.py
+++ Transportes3.py
@@ -1,6 +1,6 @@
-from openpyxl import load_workbook
-import pywhatkit
 import datetime
+import pywhatkit
+from openpyxl import load_workbook
 
 # load excel file
 path = ''
@@ -10,8 +10,8 @@ workbook = load_workbook(
 # open workbook
 sheet = workbook.active
 
-rango_condicion = sheet['A1':'AD500']
-rango_Columna = sheet['AD1':'AD500']
+rango_condicion = sheet['A1':'AD2500']
+rango_Columna = sheet['AD1':'AD2500']
 
 contador = -1
 contador2 = 1
@@ -28,25 +28,26 @@ proveedor = "proveedor"
 celular_proveedor = "celular_proveedor"
 conductor = "conductor"
 celular_conductor = "celular_conductor"
+print(sheet["AD2"].value)
 
 for i in rango_Columna:
     contador += 1
     contador2 += int(1)
-    if i[0].value == "No Enviado":
+    if i[0].value == None:
         i[0].value = "Enviado"
         ide = str(rango_condicion[contador][0].value)
-        tipo_de_servicio = rango_condicion[contador][5].value
-        fecha_servicio = rango_condicion[contador][7].value
-        hora_servicio = rango_condicion[contador][8].value
-        producto = rango_condicion[contador][9].value
-        cantidad_producto = rango_condicion[contador][10].value
-        kg = rango_condicion[contador][11].value
-        origen = rango_condicion[contador][12].value
-        destino = rango_condicion[contador][13].value
+        tipo_de_servicio = str(rango_condicion[contador][5].value)
+        fecha_servicio = str(rango_condicion[contador][7].value)
+        hora_servicio = str(rango_condicion[contador][8].value)
+        producto = str(rango_condicion[contador][9].value)
+        cantidad_producto = str(rango_condicion[contador][10].value)
+        kg = str(rango_condicion[contador][11].value)
+        origen = str(rango_condicion[contador][12].value)
+        destino = str(rango_condicion[contador][13].value)
         proveedor = str(rango_condicion[contador][14].value)
-        celular_proveedor = rango_condicion[contador][15].value
-        conductor = rango_condicion[contador][16].value
-        celular_conductor = rango_condicion[contador][17].value
+        celular_proveedor = str(rango_condicion[contador][15].value)
+        conductor = str(rango_condicion[contador][16].value)
+        celular_conductor = str(rango_condicion[contador][17].value)
         telefono = "E2YLsn9pOxH52lBJZtPeC9"
         hora = datetime.datetime.now()
         hora_real = datetime.datetime.strftime(hora, '%H')
