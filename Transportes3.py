e914341e99948e5a917431963abc734bb907d2d6
SebastianM <Sebastyk120@gmail.com>
2022-09-29 21:36:42 -0500

    Se añaden declaraciones de horas flotantes.

--- Transportes3.py
+++ Transportes3.py
@@ -1,15 +1,18 @@
 from openpyxl import load_workbook
+import pywhatkit
 
 # load excel file
+path = ''
 workbook = load_workbook(filename="ProgramacionTransporte.xlsx")
 
 # open workbook
 sheet = workbook.active
 
-rango_condicion = sheet['A1':'M308']
-rango_Columna = sheet['L1':'L308']
+rango_condicion = sheet['A1':'AD500']
+rango_Columna = sheet['AD1':'AD500']
 
 contador = -1
+contador2 = 1
 ide = "id"
 tipo_de_servicio = "tipo_de_servicio"
 fecha_servicio = "tipo_de_servicio"
@@ -24,24 +27,30 @@ celular_proveedor = "celular_proveedor"
 conductor = "conductor"
 celular_conductor = "celular_conductor"
 
-
 for i in rango_Columna:
     contador += 1
-    if i[0].value == "Hola":
-        i[0].value = "Hola"
-        ide = rango_condicion[contador][0].value
+    contador2 += 1
+    if i[0].value == "No Enviado":
+        i[0].value = "Enviado"
+        ide = str(rango_condicion[contador][0].value)
         tipo_de_servicio = rango_condicion[contador][5].value
         fecha_servicio = rango_condicion[contador][7].value
         hora_servicio = rango_condicion[contador][8].value
         producto = rango_condicion[contador][9].value
         cantidad_producto = rango_condicion[contador][10].value
-        kilogramos = rango_condicion[contador][11].value
+        kg = rango_condicion[contador][11].value
         origen = rango_condicion[contador][12].value
         destino = rango_condicion[contador][13].value
-        proveedor = rango_condicion[contador][14].value
+        proveedor = str(rango_condicion[contador][14].value)
         celular_proveedor = rango_condicion[contador][15].value
         conductor = rango_condicion[contador][16].value
         celular_conductor = rango_condicion[contador][17].value
+        telefono = "+573208831801"
+        pywhatkit.sendwhatmsg(telefono, ide + '\n' + proveedor, 22, 2, 8, True, 4)
+    else:
+        print("programa terminado")
+
+
 
 # modify he desired cell
 # sheet["L6"] = "Ejecutado123"
